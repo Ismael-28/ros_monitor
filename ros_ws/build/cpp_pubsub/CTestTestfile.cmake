914aeb3f79bf49416f9dab2d3ecf1e699d3bd5c6
Ismael-28 <796902@unizar.es>
2025-10-01 19:52:33 +0200

    Quitados ros_ws

--- ros_ws/build/cpp_pubsub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/ros_ws/cpp_pubsub
-# Build directory: /root/ros_ws/build/cpp_pubsub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
